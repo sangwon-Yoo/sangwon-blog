c3dcca6aad61254b8efd4436fe14450742677743
YooSangwon <ysw1230@gmail.com>
2024-09-23 23:24:37 +0900

    test3

--- pages/about/yoo.tsx
+++ pages/about/yoo.tsx
@@ -109,5 +109,18 @@ export default function YooPage() {
             <Footer />
         </>
     );
+}
 
+// This function gets called at build time
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts
+    //const res = await fetch('https://.../posts')
+    //const posts = await res.json()
+
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+        }
+    }
 }
