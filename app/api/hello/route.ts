c50ae9eab1d2ed977ad399068e1014ad40644248
YooSangwon <ysw1230@gmail.com>
2024-09-23 22:57:09 +0900

    test

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -2,6 +2,6 @@
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
-export function GET(request: Request, response: Response) {
+export function GET(request: Request) {
     return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
